e0bb81d40293e45782636a71dcff2774f07bfce2
bivek <bivektamang016@gmail.com>
2025-01-09 23:19:42 -0500

    Updated code for production

--- src/test/java/net/bproject/maze/maze_project/MazeProjectApplicationTests.java
+++ src/test/java/net/bproject/maze/maze_project/MazeProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MazeProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // A basic test to ensure the context loads correctly
+    }
 }
